f1aaaa2aa59ee08e596fa990955bd28fdcbc3234
Marcos-Castillo <93360137+Marcos-Castillo@users.noreply.github.com>
2023-03-05 11:15:01 -0600

    loop y direccion
    se agrega loop de juego y teclas de direccion

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -6,6 +6,8 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 /**
@@ -22,13 +24,24 @@ public class GamePanel extends JPanel implements Runnable{
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol;//768px
     final int screenHeight = tileSize * maxScreenRow;//576px
-    
+    //fps
+    int FPS=60;
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    
+    //set player defaul position
+    
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+    
     
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
     
     public void startGameThread(){
@@ -38,6 +51,50 @@ public class GamePanel extends JPanel implements Runnable{
 
     @Override
     public void run() {
+        double drawInterval=1000000000/FPS;//0.0166 seg
+        double nextDrawTime= System.nanoTime()+drawInterval;
+        
+        while (gameThread!= null){
+            
+           update();
+           repaint();
+
+            try {
+                    double remainigTime = nextDrawTime - System.nanoTime();
+                    remainigTime=remainigTime/1000000;
+                    if(remainigTime<0){
+                        remainigTime=0;
+                    }
+                    Thread.sleep((long)remainigTime);
+                    nextDrawTime+=drawInterval;
+                    
+            } catch (Exception e) {
+            }
+        }
+    }
+    
+    public void update(){
+        try {
+            if (keyH.upPressed == true) {                
+                playerY -= playerSpeed;
+            } else if (keyH.downPressed == true) {                
+                playerY += playerSpeed;
+            } else if (keyH.rightPressed == true) {                
+                playerX += playerSpeed;
+            } else if (keyH.leftPressed == true) {                
+                playerX -= playerSpeed;
+            }
+        } catch (Exception e) {
+        }
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.WHITE);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        g2.dispose();
+        
+        
         
     }
             
---
+++ src/main/KeyHandler.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ *
+ * @author Operaciones
+ */
+public class KeyHandler implements KeyListener{
+    Boolean upPressed,downPressed,leftPressed,rightPressed ;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+        if(code == KeyEvent.VK_UP){
+            upPressed = true;
+        }
+        if(code == KeyEvent.VK_DOWN){
+            downPressed = true;
+        }
+        if(code == KeyEvent.VK_LEFT){
+            leftPressed = true;
+        }
+        if(code == KeyEvent.VK_RIGHT){
+            rightPressed = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+     int code = e.getKeyCode();
+        if(code == KeyEvent.VK_UP){
+            upPressed = false;}
+        if(code == KeyEvent.VK_DOWN){
+            downPressed = false;
+        }
+        if(code == KeyEvent.VK_LEFT){
+            leftPressed = false;
+        }
+        if(code == KeyEvent.VK_RIGHT){
+            rightPressed = false;
+        }
+    }
+    
+}
