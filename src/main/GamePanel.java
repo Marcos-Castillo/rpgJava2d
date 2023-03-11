8d588dcb9d774eaa698db19dd23dc268dc0d68a2
Marcos Castillo <93360137+Marcos-Castillo@users.noreply.github.com>
2023-03-11 14:07:06 -0300

    se crearon objetos

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -12,6 +12,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import object.SuperObject;
 import tile.TileManager;
 
 
@@ -44,7 +45,11 @@ public class GamePanel extends JPanel implements Runnable{
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     public CollisionChecker cChecker = new CollisionChecker(this);
+    public AssetSetter aSetter = new AssetSetter(this);
+    
     public Player player = new Player(this, keyH);
+    public SuperObject obj[] = new SuperObject[10];
+    
     
  
     
@@ -56,6 +61,11 @@ public class GamePanel extends JPanel implements Runnable{
         this.addKeyListener(keyH);
         this.setFocusable(true);
     }
+    
+    public void setupGame() {
+    	aSetter.setObjet();
+    }
+    
     
     public void startGameThread(){
         gameThread = new Thread(this);
@@ -92,7 +102,14 @@ public class GamePanel extends JPanel implements Runnable{
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
+        
         tileM.draw(g2);
+        for(int i = 0;i<obj.length;i++) {
+        	if(obj[i]!= null) {
+        		obj[i].draw(g2, this);
+        	}
+        }
+        
         player.draw(g2);
         g2.dispose();
         
